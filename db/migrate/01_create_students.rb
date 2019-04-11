482552e1f8dd85694c0246f8556389498527336b
Susan Tan <ssusantan9@gmail.com>
2019-04-11 19:12:31 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,9 +3,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
     def change
     create_table :artists do |t|
       t.string :name
-      # t.string :genre
-      # t.integer :age
-      # t.string :hometown
     end
   end
   
